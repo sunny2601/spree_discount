fb077d9458a1b06868b43f50fdf1cbf07a701d95
sunny2601 <sunnysarrot@gmail.com>
2015-06-11 15:20:31 +0530

    Update product_decorator.rb

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -4,7 +4,7 @@ Spree::Product.class_eval do
                             :greater_than_or_equal_to => 0,
                             :less_than_or_equal_to => 100
 
-  delegate_belongs_to :master, :pre_discount_price if Variant.table_exists? && Variant.column_names.include?("pre_discount_price")
+  delegate_belongs_to :master, :pre_discount_price if Spree::Variant.table_exists? && Spree::Variant.column_names.include?("pre_discount_price")
                             
   def discount?
     discount_percentage > 0
