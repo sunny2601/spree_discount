d270af3e152a63eabc52d5372cd1306b3549f8b4
sunny2601 <sunnysarrot@gmail.com>
2015-06-11 15:29:28 +0530

    Update add_form_elements_to_product_form_right.rb

--- app/overrides/add_form_elements_to_product_form_right.rb
+++ app/overrides/add_form_elements_to_product_form_right.rb
@@ -3,4 +3,4 @@ Deface::Override.new(
   :name => "add_form_elements_to_product_form_right",
   :insert_after => "code[erb-loud]:contains('f.error_message_on :cost_price')",
   :partial => "admin/products/discount_form_elements",
-  :disabled => false)
+  )
