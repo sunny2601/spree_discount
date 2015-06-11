3d00864279d4fc8823fb59ce5832acf3cd99b204
sunny2601 <sunnysarrot@gmail.com>
2015-06-11 15:29:53 +0530

    Update add_form_elements_to_variants_form.rb

--- app/overrides/add_form_elements_to_variants_form.rb
+++ app/overrides/add_form_elements_to_variants_form.rb
@@ -3,4 +3,4 @@ Deface::Override.new(
   :name => "add_form_elements_to_variants_form",
   :insert_after => "[data-hook='cost_price']",
   :partial => "admin/variants/discount_form_elements",
-  :disabled => false)
+  )
